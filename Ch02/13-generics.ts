ada61c086760f4b669ea6ec93bf9f389430e4317
Help <pczerokey@gmail.com>
2020-09-14 17:12:51 -0400

    dynamic type referencing and identification

--- Ch02/13-generics.ts
+++ Ch02/13-generics.ts
@@ -1,11 +1,11 @@
-export class User {
+export class User<T> {
     name: string;
     age: number;
     email: string;
     isMale: boolean;
-    public classicUserData: any;
+    public classicUserData: T;
 
-    public mergeClassicUser(params: any): void {
+    public mergeClassicUser(params: T): void {
         const { name, isMale, age, email } = this;
 
         this.classicUserData = { name, isMale, age, email, ...params };
@@ -20,7 +20,7 @@ interface ClassicUser2 {
     name: { first: string, middle: string, last: string };
 }
 
-const user1 = new User();
+const user1 = new User<ClassicUser>();
 user1.mergeClassicUser({ name: { first: 'Dylan', last: 'Israel' } });
 
 const user2 = new User();
