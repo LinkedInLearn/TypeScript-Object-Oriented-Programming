26aa19274cc16893f41515d877735eeaea944c9b
Help <pczerokey@gmail.com>
2020-09-14 17:14:32 -0400

    ch12 challenge completion

--- Ch02/challenge-inheritance-2.ts
+++ Ch02/challenge-inheritance-2.ts
@@ -4,7 +4,7 @@
  * -----------------------------
  * 1. Use the User class below.
  * 2. Create an Admin class that extends User.
- * 3. Create a Guest class that implements User. 
+ * 3. Create a Guest class that implements User.
  * 4. Have both classes below pass firstName, lastName and email in their constructors.
  */
 
@@ -19,5 +19,35 @@ export class User {
 
     doesEmailMatch(email: string): boolean {
         return this.email === email;
+    }
+}
+
+export class Admin extends User {
+
+    constructor(firstName: string, lastName: string, email: string) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
+
+export class Guest implements User {
+    firstName: string;
+    lastName: string;
+    email: string;
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    doesEmailMatch(email: string): boolean {
+        return this.email === email;
+    }
+
+    constructor(firstName: string, lastName: string, email: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 }
